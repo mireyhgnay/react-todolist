0da22641df76ad342341121872c6d1875b718203
hyerim <hyerimiya1216@gmail.com>
2023-10-30 23:48:20 +0900

    Feat : 투두리스트 기능 구현 완료

--- todolist/src/components/TodoCreate.jsx
+++ todolist/src/components/TodoCreate.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -77,16 +78,41 @@ const Input = styled.input`
 
 function TodoCreate() {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
 
   // open 의 상태를 변경하는 함수
   const onToggle = () => setOpen(!open);
+  // input 상태 변경
+  const onChange = (e) => setValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "CREATE",
+      // todo 배열에 추가되는 것
+      todo: {
+        id: nextId.current, // useRef(5)
+        text: value, // input value 값
+        done: false, // 초기는 false
+      },
+    });
+    setValue("");
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
-            <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter 를 누르세요"
+              onChange={onChange}
+              value={value}
+            />
           </InsertForm>
         </InsertFormPositioner>
       )}
@@ -97,4 +123,4 @@ function TodoCreate() {
   );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
